e6467a2bee6ab6fedcc29ec03b0e525a36b1171e
Iris Muñoz <imunoz@conwet.com>
2015-03-20 20:19:32 +0100

    New main.js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 /*
- *     Copyright (c) 2014 CoNWeT Lab., Universidad Politécnica de Madrid
+ *     Copyright (c) 2014-2015 CoNWeT Lab., Universidad Politécnica de Madrid
  *
  *     This file is part of the flotr2-graph widget.
  *
@@ -35,126 +35,14 @@
  *
  */
 
-/*global Flotr, MashupPlatform*/
+/*global $, Widget */
 
-(function () {
+
+$(function () {
 
     "use strict";
 
-    var data = [];
-    var graphContainer;
-    var loadLayer;
-    var original_config;
-    var current_config;
+    var wgt = new Widget();
+    wgt.init();
 
-    var process_input = function process_input(input) {
-        var info;
-
-        info = JSON.parse(input);
-
-        if (typeof info == 'string') {
-            // Loading msg
-            switch (info) {
-            case 'START':
-                // init loading mode
-                loadLayer.classList.add('on');
-                break;
-            case 'END':
-                // exit loading mode
-                loadLayer.classList.remove('on');
-                break;
-            case 'RESET':
-                data = [];
-                break;
-            }
-            return;
-        }
-
-        if ('config' in info) {
-            process_config(info.config);
-        }
-
-        if ('datasets' in info) {
-            process_new_datasets(info.datasets);
-        }
-
-        if ('data' in info) {
-            process_new_data(info.data);
-        }
-    };
-
-    var process_config = function process_config(new_config) {
-        original_config = new_config;
-        current_config = Flotr._.clone(new_config);
-        data = [];
-    };
-
-    var process_new_datasets = function process_new_datasets(new_datasets) {
-        for (var key in new_datasets) {
-            data[key] = new_datasets[key];
-            data[key].data = [];
-        }
-    };
-
-    var process_new_data = function process_new_data(new_data) {
-        for (var key in new_data) {
-            if (!(key in data)) {
-                data[key] = {data: new_data[key]};
-            } else {
-                data[key].data = data[key].data.concat(new_data[key]);
-            }
-        }
-
-        draw_graph();
-    };
-
-    var draw_graph = function draw_graph() {
-        Flotr.draw(graphContainer, data, current_config);
-    };
-
-    MashupPlatform.widget.context.registerCallback(function (new_values) {
-        var hasChanged;
-
-        if ('heightInPixels' in new_values) {
-            document.getElementById('linearGraphContainer').style.height = (document.body.getBoundingClientRect().height - 16) + "px";
-            hasChanged = true;
-        }
-
-        if ('widthInPixels' in new_values) {
-            document.getElementById('linearGraphContainer').style.width = (document.body.getBoundingClientRect().width - 10) + "px";
-            hasChanged = true;
-        }
-
-        if (hasChanged) {
-            draw_graph();
-        }
-    });
-
-    window.addEventListener('load', function () {
-        // Resize the linearGraphContainer
-        document.getElementById('linearGraphContainer').style.height = (document.body.getBoundingClientRect().height - 16) + "px";
-        document.getElementById('linearGraphContainer').style.width = (document.body.getBoundingClientRect().width - 10) + "px";
-        loadLayer = document.getElementById('loadLayer');
-        graphContainer = document.getElementById('linearGraphContainer');
-
-        draw_graph();
-        Flotr.EventAdapter.observe(graphContainer, 'flotr:select', function (area) {
-            // Draw graph with new area
-            current_config = Flotr._.extend(current_config, {
-                xaxis: {min: area.x1, max: area.x2},
-                yaxis: {min: area.y1, max: area.y2}
-            });
-            draw_graph();
-        });
-
-        Flotr.EventAdapter.observe(graphContainer, 'flotr:click', function () {
-            // When graph is clicked, draw the graph with default area.
-            current_config = Flotr._.clone(original_config);
-            draw_graph();
-        });
-    }, true);
-
-    // Input handler
-    MashupPlatform.wiring.registerCallback('input', process_input);
-
-})();
+});
